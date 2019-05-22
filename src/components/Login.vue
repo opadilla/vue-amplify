3d314cdf3d22002041ed68ba91ced062a9f548cf
Omar <omar.padilla@integrify.com>
2019-05-22 14:46:54 -0500

    hard code amplify auth to integrifydev

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,22 +7,34 @@
 </template>
 
 <script>
-import Amplify, { Auth } from 'aws-amplify';
-import { AmplifyEventBus } from 'aws-amplify-vue';
+import Amplify, { Auth } from "aws-amplify";
+import { AmplifyEventBus } from "aws-amplify-vue";
+
+const awsExportsIntegrifydev = {
+  aws_project_region: "us-east-1",
+  aws_user_pools_id: "us-east-1_YZACgTdcm",
+  aws_cognito_region: "us-east-1",
+
+  aws_cognito_identity_pool_id: "us-east-1:552d8e00-58b5-4cb7-94ed-20ba0602b52d",
+  
+  
+  aws_user_pools_web_client_id: "kh7od7ma26k8naj5l7p0cig2v",
+  oauth: {}
+};
+
+Amplify.configure(awsExportsIntegrifydev);
 
 export default {
-  name: 'Login',
+  name: "Login",
   props: {
     msg: String,
     signedIn: false
   },
   created() {
     this.findUser();
-    AmplifyEventBus.$on('authState', info => {
-      if (info === "signedIn") 
-        this.findUser();
-      else
-        this.signedIn = false;
+    AmplifyEventBus.$on("authState", info => {
+      if (info === "signedIn") this.findUser();
+      else this.signedIn = false;
     });
   },
   methods: {
@@ -31,12 +43,12 @@ export default {
         const user = await Auth.currentAuthenticatedUser();
         this.signedIn = true;
         console.log(user);
-      } catch(err) {
+      } catch (err) {
         this.signedIn = false;
       }
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
