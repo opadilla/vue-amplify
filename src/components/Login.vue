e1bbbdb1f443ffb6b844b33d58bb9784a20460ef
Omar <omar.padilla@integrify.com>
2019-05-22 14:55:03 -0500

    cleanup console errs

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -15,22 +15,25 @@ const awsExportsIntegrifydev = {
   aws_user_pools_id: "us-east-1_YZACgTdcm",
   aws_cognito_region: "us-east-1",
 
-  aws_cognito_identity_pool_id: "us-east-1:552d8e00-58b5-4cb7-94ed-20ba0602b52d",
-  
-  
+  aws_cognito_identity_pool_id:
+    "us-east-1:552d8e00-58b5-4cb7-94ed-20ba0602b52d",
+
   aws_user_pools_web_client_id: "kh7od7ma26k8naj5l7p0cig2v",
   oauth: {}
 };
 
-Amplify.configure(awsExportsIntegrifydev);
-
 export default {
   name: "Login",
   props: {
-    msg: String,
-    signedIn: false
+    msg: String
+  },
+  data: function() {
+    return {
+      signedIn: false
+    };
   },
   created() {
+    Amplify.configure(awsExportsIntegrifydev);
     this.findUser();
     AmplifyEventBus.$on("authState", info => {
       if (info === "signedIn") this.findUser();
